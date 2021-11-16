a1649e41bc708f12ee3b8fe1ce2ee24b804ef2e1
akiibajin <julianrobayo8000@gmail.com>
2021-11-15 20:15:34 -0500

    material-ui icons install and answers styles

--- client/src/Components/AnswerForm.jsx
+++ client/src/Components/AnswerForm.jsx
@@ -1,14 +1,33 @@
-export default function AnswerForm({answerGenerator}) {
-    const handleSubmit=(event)=>{
-        event.preventDefault()
-        answerGenerator(event.target[0].value)
+import { TextField, Button , Box } from "@mui/material";
+import { useForm, Controller } from "react-hook-form";
+export default function AnswerForm({ answerGenerator }) {
+    const {control,reset,formState:{errors},handleSubmit} =useForm()
+    const handleOnSubmit=(data)=>{
+        console.log(data)
+        answerGenerator(data.answerToSend)
+        reset({
+            answerToSend:""
+        })
     }
-    return(
-        <div>
-            <form onSubmit={handleSubmit}>
-                <textarea placeholder="Here the answerContent"/>
-                <input type="submit" value="Submit Your Answer"/>
-            </form>
-        </div>
-    )
-};
+    return (
+    <Box>
+        <form onSubmit={handleSubmit(handleOnSubmit)}>
+            <Controller
+            name="answerToSend"
+            control={control}
+            defaultValue=""
+            rules={{ required:{value:true,message:"Input Required"}}}
+            render={({field})=><TextField
+            {...field}
+            color="secondary"
+            label="Type your answer"
+            multiline
+            rows={6}
+            sx={{width:"60vw"}}
+            />}
+            />
+            <Button color="info" type="submit" variant="contained">Submit Your Answers</Button>
+        </form>
+    </Box>
+  );
+}
--- client/src/Components/AnswerTemplate.jsx
+++ client/src/Components/AnswerTemplate.jsx
@@ -1,18 +1,28 @@
 import { useDispatch } from "react-redux";
 import { changeAnswer } from "../Redux/Actions/answerAction";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import { Button, Typography } from "@mui/material";
+import "../Styles/answerTemplate.css";
 export default function AnswerTemplate({ content, theBest, id, idQuestion }) {
   const dispatch = useDispatch();
   const handleClick = () => {
     dispatch(changeAnswer(id, idQuestion));
   };
   return (
-    <div>
-      <p>{content}</p>
-      {theBest ? <p>!</p> : <p>-</p>}
+    <div className="answerChildren">
+      <span>{content}</span>
       {theBest ? (
-        <p></p>
+        <CheckBoxIcon fontSize="large" color="success" />
       ) : (
-        <button onClick={handleClick}>Check as a Correct Answer</button>
+        <button
+          variant="contained"
+          color="info"
+          onClick={handleClick}
+        >
+          <Typography>
+          Correct
+          </Typography>
+        </button>
       )}
     </div>
   );
