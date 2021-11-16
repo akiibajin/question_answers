9909d8e4304a13a01eaa63d1327e72e4e9104a41
akiibajin <julianrobayo8000@gmail.com>
2021-11-15 22:12:39 -0500

    docker api

--- api/src/db/postgres/db.js
+++ api/src/db/postgres/db.js
@@ -2,26 +2,30 @@ const { Sequelize, Op } = require("sequelize");
 
 const userModel = require("../../cores/user/model/userPostgres");
 const questionModel = require("../../cores/questions/model/questionPostgres");
-const answerModel = require("../../cores/answers/model/answerPostgres")
+const answerModel = require("../../cores/answers/model/answerPostgres");
 require("dotenv").config();
 
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_DBNAME = process.env.DB_DBNAME
-const DB_DATABASE = process.env.DB_DATABASE
-
-const sequelize= new Sequelize(`${DB_DBNAME}://${DB_USERNAME}:${DB_PASSWORD}@/${DB_DATABASE}`)
-// const sequelize = new Sequelize(
-//   `${DB_DBNAME}://${DB_USERNAME}:${DB_PASSWORD}@${process.env.PSQL_HOST}:5432/${DB_DATABASE}`);
+const DB_DBNAME = process.env.DB_DBNAME;
+const DB_DATABASE = process.env.DB_DATABASE;
+const DB_ROUTE = process.env.DB_ROUTE;
+let sequelize=null
+if (DB_ROUTE) {
+   sequelize = new Sequelize(DB_ROUTE);
+} else {
+   sequelize = new Sequelize(
+    `${DB_DBNAME}://${DB_USERNAME}:${DB_PASSWORD}@${process.env.PSQL_HOST}:5432/${DB_DATABASE}`
+  );
+}
 
 const User = userModel(sequelize);
 const Question = questionModel(sequelize);
 const Answer = answerModel(sequelize);
 
-User.hasMany(Question)
-User.hasMany(Answer)
-Question.hasMany(Answer)
-
+User.hasMany(Question);
+User.hasMany(Answer);
+Question.hasMany(Answer);
 
 module.exports = {
   db: sequelize,
@@ -30,4 +34,3 @@ module.exports = {
   Answer,
   Op,
 };
-
